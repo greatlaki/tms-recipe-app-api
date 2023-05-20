1ec91f6c50bcdfd5c5b2da9c7391ca60eb157811
vadim.rudenkov <vadim.rudenkov@muehlemann-popp.ch>
2023-05-20 16:45:39 +0300

    implementing ingredients API

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -2,12 +2,13 @@
 URL mappings for the recipe app
 """
 from django.urls import include, path
-from recipe.views import RecipeViewSet, TagViewSet
+from recipe.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register("recipes", RecipeViewSet)
 router.register("tags", TagViewSet)
+router.register("ingredients", IngredientViewSet)
 
 app_name = "recipe"
 
